5f4a0c7ef02bfdfe81ffaca10656bf943de8d9a7
TomerBu <tomer.bu@gmail.com>
2017-03-22 13:15:12 +0200

    Initial commit

---
+++ app/src/main/java/android/itomerbu/layoutdemo/PrayerTimeFragement.java
@@ -0,0 +1,29 @@
+package android.itomerbu.layoutdemo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PrayerTimeFragement extends Fragment {
+
+
+    public PrayerTimeFragement() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_prayer_time_fragement, container, false);
+    }
+
+}
